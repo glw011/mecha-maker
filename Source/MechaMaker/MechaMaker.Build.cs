6f411e3e88f7312eb50bc0bce0d36a8be491c1c3
glw011 <glw011@email.latech.edu>
2026-03-09 11:17:54 -0500

    fixed issue with project not building because I forgot to include slate as dependency in project's build.cs

--- Source/MechaMaker/MechaMaker.Build.cs
+++ Source/MechaMaker/MechaMaker.Build.cs
@@ -6,7 +6,7 @@ public class MechaMaker : ModuleRules{
 	public MechaMaker(ReadOnlyTargetRules Target) : base(Target){
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "UMG"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
